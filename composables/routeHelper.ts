714f1dbf0658ccab628270aaadf21db63b824e5d
mwiguna <mwiguna33@gmail.com>
2022-10-04 19:07:10 +0700

    route to server

--- composables/routeHelper.ts
+++ composables/routeHelper.ts
@@ -1,6 +1,6 @@
 export default function () {
     let baseUrl = 'https://wiguna.cendikiawandigital.com/apitest/ranker/'
-    baseUrl = 'http://localhost/project/apitest/ranker/'
+    // baseUrl = 'http://localhost/project/apitest/ranker/'
     
     return {
         "usersUrl": baseUrl + "user/getUsers",
